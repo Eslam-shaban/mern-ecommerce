3999ce332b1066dd833229c37d437351e5beee60
Eslam-shaban <73853163+Eslam-shaban@users.noreply.github.com>
2025-07-03 13:24:20 +0300

    feat( Order Details Page)

--- frontend/src/pages/public/OrderDetails.jsx
+++ frontend/src/pages/public/OrderDetails.jsx
@@ -1,11 +1,74 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import API from "../../api/axiosInstance";
 
 const OrderDetails = () => {
+    const { orderId } = useParams();
+    const [order, setOrder] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchOrder = async () => {
+            try {
+                const res = await API.get(`/orders/${orderId}`);
+                setOrder(res.data.order);
+            } catch (error) {
+                console.error("Error fetching order:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchOrder();
+    }, [orderId]);
+
+    if (loading) return <p className="p-4">Loading...</p>;
+    if (!order) return <p className="p-4 text-red-500">Order not found</p>;
+
     return (
-        <div>
-            Order Details
+        <div className="max-w-5xl mx-auto p-6 space-y-6">
+            <h1 className="text-2xl font-bold">Order #{order._id}</h1>
+
+            {/* Shipping Info */}
+            <div className="bg-white p-4 shadow-lg rounded">
+                <h2 className="text-lg font-semibold mb-2">Shipping Info</h2>
+                <p><strong>Name:</strong> {order.user.name}</p>
+                <p><strong>Email:</strong> {order.user.email}</p>
+                <p><strong>Address:</strong> {order.shippingAddress.address}, {order.shippingAddress.city}, {order.shippingAddress.state}, {order.shippingAddress.country}</p>
+            </div>
+
+            {/* Payment */}
+            <div className="bg-white p-4 shadow-lg rounded">
+                <h2 className="text-lg font-semibold mb-2">Payment</h2>
+                <p><strong>Method:</strong> {order.paymentMethod}</p>
+                <p><strong>Status:</strong> {order.paymentStatus}</p>
+            </div>
+            {/* Items */}
+            <div className="bg-white p-4 shadow-lg rounded">
+                <h2 className="text-lg font-semibold mb-2">Items</h2>
+                <ul className="divide-y divide-gray-500">
+                    {order.orderItems.map((item) => (
+                        <li key={item.product} className="py-3 flex justify-between items-center">
+                            <div className="flex items-center gap-4">
+                                <img src={item.images[0]} alt={item.name} loading="lazy" className="w-16 h-16 object-cover rounded" />
+                                <span>{item.name}</span>
+                            </div>
+                            <div>
+                                {item.quantity} × EGP {item.price.toFixed(2)} = <strong>EGP {(item.quantity * item.price).toFixed(2)}</strong>
+                            </div>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+
+            {/* Total */}
+            <div className="bg-white p-4 shadow-lg rounded text-left">
+                <p><strong>Items Price:</strong> EGP {order.itemsPrice.toFixed(2)}</p>
+                <p><strong>Shipping:</strong> EGP {order.shippingPrice.toFixed(2)}</p>
+                <p className="text-lg font-bold"><strong>Total:</strong> EGP {order.totalPrice.toFixed(2)}</p>
+            </div>
         </div>
     );
-}
+};
 
 export default OrderDetails;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -23,6 +23,7 @@ export default {
             },
             boxShadow: {
                 custom: '0 4px 12px rgba(0, 0, 0, 0.08)', // typical modern card shadow
+                custon1: 'rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px';
 
             }
         },
