c8b0f2849f59380a70c0f6f58fa3c90da86897fc
Eslam-shaban <73853163+Eslam-shaban@users.noreply.github.com>
2025-07-20 14:41:52 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,13 +22,14 @@ const app = express();
 
 // Middleware
 app.use(express.json()); // Parses incoming JSON requests
-app.use(cors({
-    // origin: "http://localhost:3000", // Allow requests from your frontend
-    origin: "https://mern-ecommerce-three-kappa.vercel.app/", //  frontend live url
-    credentials: true // Allow cookies and authorization headers
-}));
+// app.use(cors({
+//     // origin: "http://localhost:3000", // Allow requests from your frontend
+//     origin: "https://mern-ecommerce-three-kappa.vercel.app/", //  frontend live url
+//     credentials: true // Allow cookies and authorization headers
+// }));
+app.use(cors());
 
-app.use('/api/v1/products', productRoutes)
+app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/users', authRoutes);
 app.use('/api/v1/orders', orderRoutes); //  Use order routes
 app.use('/api/v1/stripe', stripeRoutes);
